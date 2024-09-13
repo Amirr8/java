beb1c264955d0e1fc424f2a88a27cdf29a17e4db
Amirr8 <164495490+Amirr8@users.noreply.github.com>
2024-09-13 02:24:49 -0700

    Add files via upload

--- Stack_Ramac.java
+++ Stack_Ramac.java
@@ -10,24 +10,26 @@ public class Stack_Ramac {
     public static void main(String[] args) {
        Stack<String> stack = new Stack<>();
        int maximumSize=5;
-     
- 
-       System.out.println("Stack is " + (stack.size() >= maximumSize ? "MAX":" NOT MAX"));
+       if(stack.size() <= maximumSize){
        stack.push("Apple");
        stack.push("Banana");
        stack.push("Cherry");
        stack.push("Date");
        stack.push("Elderberry");
+}else{
+System.out.println("Stack limit full");
+}
+   
        
        System.out.println("Top element is: " + (stack.isEmpty()?"Stack is empty": stack.peek()));
        
        String elementToSearch="Banana";
        int position=stack.search(elementToSearch);
-       System.out.println("Element \""+elementToSearch + "\" position " + (position > 0? position :"not found"));
+       System.out.println("Element \""+elementToSearch + "\" found at position " + (position > 0? position :"not found"));
        
        String elementToSearch2="Grape";
        int position2=stack.search(elementToSearch2);
-       System.out.println("Element \""+elementToSearch2 + "\" position " + (position2 > 0? position2 :"not found"));
+       System.out.println("Element \""+elementToSearch2 + "\" found at position " + (position2 > 0? position2 :"not found"));
        
        
        System.out.println("popped Elelment " + (stack.isEmpty()? " is empty" : stack.pop()));
@@ -35,9 +37,8 @@ public class Stack_Ramac {
        System.out.println("popped Elelment " + (stack.isEmpty()? " is empty" : stack.pop()));
        System.out.println("popped Elelment " + (stack.isEmpty()? " is empty" : stack.pop()));
        System.out.println("popped Elelment " + (stack.isEmpty()? " is empty" : stack.pop()));
-       System.out.println("popped Elelment " + (stack.isEmpty()? " is empty" : stack.pop()));
        
-       
+       System.out.println("Stack is " + (stack.isEmpty()?"empty":" not empty"));
     }
     
 }
